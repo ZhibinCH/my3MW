ba54d163c9d30dbcf4333c86fe7b42a23604f7f7
Zhibin Cheng <zhibin.cheng@tum.de>
2017-08-06 10:43:55 +0200

    Update the models of sites and its corresponding views and html template

--- sites/models.py
+++ sites/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
+#class Sites(models.Model):
+#    site_name = models.CharField(max_length=30)
+#    site_id = models.CharField(max_length=30)
+#    date = models.DateTimeField()
+#    a_value = models.DecimalField(max_digits=5, decimal_places=2)
+#    b_value = models.DecimalField(max_digits=5, decimal_places=2)
+
 class Sites(models.Model):
-    site_name = models.CharField(max_length=30)
-    site_id = models.CharField(max_length=30)
-    date = models.DateTimeField()
-    a_value = models.DecimalField(max_digits=5, decimal_places=2)
-    b_value = models.DecimalField(max_digits=5, decimal_places=2)
+    name = models.CharField(max_length=30)
+
 
