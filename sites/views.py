1b9031bd97138516c5b2ff3604acb72ee432e981
Zhibin Cheng <zhibin.cheng@tum.de>
2017-08-09 15:35:17 +0200

    Modify the site_detail function into a clear structure and update its corresponding html file

--- sites/views.py
+++ sites/views.py
@@ -1,33 +1,22 @@
 from django.http import HttpResponse
 from django.template import loader
-from django.shortcuts import render,render_to_response
-from django.http import Http404
 from sites.models import *
 
 def sites_overview(request):
     
-#    if not Sites.objects.all():
-#
-#         site = Sites(site_name='Demo Site', site_id='1',date='2015-02-01',a_value = '12',b_value='16')
-#         site.save()
-#         site = Sites(site_name='Demo Site', site_id='1',date='2015-02-03',a_value = '20',b_value='100')
-#         site.save()
-#         site = Sites(site_name='Demo Site', site_id='1',date='2015-02-10',a_value = '20',b_value='80')
-#         site.save()
-#         site = Sites(site_name='ABC Site', site_id='2',date='2015-02-03',a_value = '5',b_value='15')
-#         site.save()
-#         site = Sites(site_name='XYZ Site', site_id='3',date='2015-02-15',a_value = '5',b_value='15')
-#         site.save()
-#         site = Sites(site_name='XYZ Site', site_id='3',date='2015-02-28',a_value = '5',b_value='15')
-#         site.save()
-
     template = loader.get_template('sites/sites_list.html')
     context = {}
     context['sites'] = Sites.objects.all()
     return HttpResponse(template.render(context, request))
-#    return render_to_response('sites/sites_list.html')
 
 
 def site_detail(request,site_id):
+    
+    template = loader.get_template('sites/site_detail.html')
+    context = {}
     site = Site.objects.filter(site_id = site_id)
-    return render_to_response('sites/site_detail.html',{'site':site})
+    site_name = Sites.objects.get(id = site_id).name  
+    context['site'] = site
+    context['site_name'] = site_name
+    return HttpResponse(template.render(context, request))
+
