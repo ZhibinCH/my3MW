c4af64aa665e7629ac4493ebb4aff80f45896ae4
Zhibin Cheng <zhibin.cheng@tum.de>
2017-08-14 11:33:40 +0200

    Modify the Models, so that it fits the Django Admin better

--- sites/models.py
+++ sites/models.py
@@ -1,19 +1,20 @@
 from django.db import models
 
-#class Sites(models.Model):
-#    site_name = models.CharField(max_length=30)
-#    site_id = models.CharField(max_length=30)
-#    date = models.DateTimeField()
-#    a_value = models.DecimalField(max_digits=5, decimal_places=2)
-#    b_value = models.DecimalField(max_digits=5, decimal_places=2)
-
 class Sites(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField('Site name',max_length=30)
+    
+    def __str__(self):              # __unicode__ on Python 2
+        return self.name
+    class Meta:
+        verbose_name_plural = "Site Names"
 
 class Site(models.Model):
     site_id = models.ForeignKey(Sites, on_delete=models.CASCADE)
-    date = models.DateTimeField('Date')
+    date = models.DateField('Date')
     a_value = models.DecimalField('A Value',max_digits=5, decimal_places=2)
     b_value = models.DecimalField('B Value',max_digits=5, decimal_places=2)
+    
+    class Meta:
+        verbose_name_plural = "Site Items"
 
 
