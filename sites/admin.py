e444bb5ff7a50049f6022df92a889acf1a8372e1
Zhibin Cheng <zhibin.cheng@tum.de>
2017-08-14 11:34:05 +0200

    Use the Django Admin

--- sites/admin.py
+++ sites/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from sites.models import *
+
+@admin.register(Sites)
+class SitesAdmin (admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Site)
+class SiteAdmin (admin.ModelAdmin):
+    list_display = ('site_id','date','a_value','b_value')
+    #list_editable = ('date','a_value','b_value') # Make the item editable directly in the list
 
 # Register your models here.
